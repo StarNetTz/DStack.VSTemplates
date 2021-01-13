9353b1677e7651eb58548ab8b6fcb0827cde3882
Zvjezdan Tomicevic <zeko77@gmail.com>
2021-01-13 20:01:13 +0100

    Converted WebApi unit tests to xUnit

---
+++ TemplateDomain/TemplateDomain.WebApi.UnitTests/QueryServiceUnitTests/LookupsTests/LookupsServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using TemplateDomain.ReadModel;
+using TemplateDomain.WebApi.ServiceInterface;
+using TemplateDomain.WebApi.ServiceModel;
+using System.Threading.Tasks;
+using Funq;
+using ServiceStack;
+using Moq;
+using Xunit;
+using System.Linq;
+
+namespace TemplateDomain.WebApi.UnitTests.LookupsServiceTests
+{
+    public class LookupsServiceTests : IClassFixture<TestFixture<LookupsService>>
+    {
+        LookupsService Service;
+
+        public LookupsServiceTests(TestFixture<LookupsService> fixture)
+        {
+            Service = fixture.Service;
+        }
+
+        [Fact]
+        public async Task Should_GetLookup()
+        {
+            var req = new GetLookup();
+            var response = await Service.Any(req) as Lookup;
+            var doc = response.Data.First();
+
+            Assert.Equal("1", doc.Id);
+            Assert.Equal("USA", doc.Value);
+        }
+    }
+
+    public class TestFixture<T> : QueryServiceFixtureBase<T> where T : Service
+    {
+        public override void RegisterServices(Container container)
+        {
+            container.Register(CreateQueryByIdMock());
+        }
+
+            static IQueryById CreateQueryByIdMock()
+            {
+                var queryByIdMock = new Mock<IQueryById>();
+                queryByIdMock.Setup(x => x.GetById<Lookup>(It.IsAny<string>())).ReturnsAsync(new Lookup
+                {
+                    Id = "Lookups-Countries",
+                    Data = new System.Collections.Generic.List<LookupItem> {
+                    new LookupItem { Id = "1", Value = "USA" }
+                }
+                });
+                return queryByIdMock.Object;
+            }
+    }
+}
---
+++ TemplateDomain/TemplateDomain.WebApi.UnitTests/QueryServiceUnitTests/TypeaheadsTests/TypeaheadsServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using TemplateDomain.ReadModel;
+using TemplateDomain.WebApi.ServiceInterface;
+using TemplateDomain.WebApi.ServiceModel;
+using System.Threading.Tasks;
+using Xunit;
+using ServiceStack;
+using Moq;
+using Funq;
+
+namespace TemplateDomain.WebApi.UnitTests.TypeaheadsServiceTests
+{
+    public class TypeaheadsServiceTests : IClassFixture<TestFixture<TypeaheadQueryService>>
+   {
+        TypeaheadQueryService Service;
+
+        public TypeaheadsServiceTests(TestFixture<TypeaheadQueryService> f)
+        {
+            Service = f.Service;
+        }
+
+        [Fact]
+       public async Task can_execute_request()
+       {
+           var req = new FilterTypeahead();
+           var response = await Service.Any(req) as PaginatedResult<TypeaheadItem>;
+           Assert.NotNull(response);
+       }
+   }
+
+    public class TestFixture<T> : QueryServiceFixtureBase<T> where T : Service
+    {
+        public override void RegisterServices(Container container)
+        {
+            container.Register(CreateIOrganizationSearchQueryMock());
+        }
+
+            static ITypeaheadSearchQuery CreateIOrganizationSearchQueryMock()
+            {
+                var queryByIdMock = new Mock<ITypeaheadSearchQuery>();
+                queryByIdMock.Setup(x => x.Execute(It.IsAny<ISearchQueryRequest>())).ReturnsAsync(new PaginatedResult<TypeaheadItem>());
+                return queryByIdMock.Object;
+            }
+    }
+}
