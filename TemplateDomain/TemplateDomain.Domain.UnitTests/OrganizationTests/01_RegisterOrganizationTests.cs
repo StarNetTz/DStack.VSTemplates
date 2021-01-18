a2f9cc77f696dd727ddf635e16ecfb8b5d1c2699
Zvjezdan Tomicevic <zeko77@gmail.com>
2021-01-18 11:40:17 +0100

    Cleanup

--- TemplateDomain/TemplateDomain.Domain.UnitTests/OrganizationTests/01_RegisterOrganizationTests.cs
+++ TemplateDomain/TemplateDomain.Domain.UnitTests/OrganizationTests/01_RegisterOrganizationTests.cs
@@ -28,7 +28,7 @@ namespace TemplateDomain.Domain.UnitTests.OrganizationTests
         }
 
         [Fact]
-        public async Task NonIdempotent_Command__Should_Throw_DomainError()
+        public async Task NonIdempotent_Command_Should_Throw_DomainError()
         {
             string aggId = "Organizations-1";
             var cmd = CommandsFactory.CreateRegisterOrganizationCommand(aggId);
---
+++ templates/src/ItemTemplates/DStackAggregate/CompanyAggregate.cs
@@ -0,0 +1,35 @@
+﻿using $safeprojectname$.PL.Commands;
+using $safeprojectname$.PL.Events;
+using DStack.Aggregates;
+
+namespace $rootnamespace$.$fileinputname$
+{
+    public class $fileinputname$Aggregate : Aggregate
+    {
+        $fileinputname$AggregateState State;
+
+        public $fileinputname$Aggregate($fileinputname$AggregateState state) : base(state)
+            => State = state;
+
+        internal void Create$fileinputname$(Create$fileinputname$ c)
+        {
+            if (State.Version > 0)
+                if (IsIdempotent(c))
+                    return;
+                else
+                    throw DomainError.Named("$fileinputname$AlreadyExists", string.Empty); 
+    
+            var e = new $fileinputname$Created() 
+			{ 
+				Id = c.Id, 
+				IssuedBy = c.IssuedBy, 
+				TimeIssued = c.TimeIssued,
+                Name = c.Name
+            };
+            Apply(e);
+        }
+
+            bool IsIdempotent(Create$fileinputname$ c)
+                => c.Name == State.Name;
+    }
+}
