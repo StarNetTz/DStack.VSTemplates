1aa6cf976435d017b18a01e07e1908f2658c25e9
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-02-15 12:00:23 +0100

    NET 6 update

--- TemplateDomain/TemplateDomain.WebApi/Program.cs
+++ TemplateDomain/TemplateDomain.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using ServiceStack;
@@ -8,14 +9,20 @@ namespace TemplateDomain.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(builder =>
-                {
-                    builder.UseModularStartup<Startup>();
-                });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+
+            app.Run();
+        }
     }
 }
