48247d0b65d61e84e1bf4f24ee9cf8f85b87b09b
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-02-15 15:42:03 +0100

    Update completed

--- templates/src/SolutionTemplate/TemplateDomain.ReadModel/SearchQueryRequest.cs
+++ templates/src/SolutionTemplate/TemplateDomain.ReadModel/SearchQueryRequest.cs
@@ -13,6 +13,6 @@
         public string Collection { get; set; }
         public string Qry { get; set; }
         public int CurrentPage { get; set; }
-        public int PageSize { get; set; }
+        public int PageSize { get; set; } = 1;
     }
 }
