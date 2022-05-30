f70915b69af23ded33efec23dcc6c951bd39b668
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-05-30 15:58:58 +0200

    Progress

--- TemplateDomain/TemplateDomain.Common.UnitTests/RecordDictionaryTests.cs
+++ TemplateDomain/TemplateDomain.Common.UnitTests/RecordDictionaryTests.cs
@@ -1,18 +1,19 @@
-﻿using Xunit;
+﻿using TemplateDomain.Testing;
+using Xunit;
 
 namespace TemplateDomain.Common.UnitTests
 {
-    public class RecordDictionaryTests
+    public partial class RecordDictionaryTests
     {
         [Fact]
         public void Should_Use_Value_Based_Equality_Test()
         {
-            var a = new RecordDictionary<string, Address> { { "a", new Address { City = "London" } } };
-            var b = new RecordDictionary<string, Address> { { "a", new Address { City = "London" } } };
-            var c = new RecordDictionary<string, Address> { { "c", new Address { City = "London" } } };
+            var a = new RecordDictionary<string, Address> { { "a", CommonTestData.CreateAddress() } };
+            var b = new RecordDictionary<string, Address> { { "a", CommonTestData.CreateAddress() } };
+            var c = new RecordDictionary<string, Address> { { "c", CommonTestData.CreateAddress() } };
 
-            object e = new RecordDictionary<string, Address> { { "a", new Address { City = "London" } } };
-            object f = new RecordDictionary<string, Address> { { "a", new Address { City = "London" } } };
+            object e = new RecordDictionary<string, Address> { { "a", CommonTestData.CreateAddress() } };
+            object f = new RecordDictionary<string, Address> { { "a", CommonTestData.CreateAddress() } };
 
             RecordDictionary<string, Address> d = null;
 
