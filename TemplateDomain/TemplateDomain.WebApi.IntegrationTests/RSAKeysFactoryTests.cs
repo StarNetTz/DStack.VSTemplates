9aa3b633aa8e406ab8f7b69999639c51c8a5f5c8
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-10-13 11:18:57 +0200

    Added RSAKey generator in integration tests

---
+++ TemplateDomain/TemplateDomain.WebApi.IntegrationTests/RSAKeysFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack;
+using Xunit;
+
+namespace TemplateDomain.WebApi.IntegrationTests
+{
+    public class RSAKeysFactoryTests
+    {
+        [Fact]
+        public void ShouldCreatePrivateAndPublicRSA2048Keys()
+        {
+            var privateKey = RsaUtils.CreatePrivateKeyParams(RsaKeyLengths.Bit2048);
+            var publicKey = privateKey.ToPublicRsaParameters();
+            var privateKeyXml = privateKey.ToPrivateKeyXml();
+            var publicKeyXml = privateKey.ToPublicKeyXml();
+        }
+    }
+}
